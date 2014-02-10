8d44b86d5e21c281f067086e31a6a702af2bf88b
Erik Granlund <egranlund@unifiedcompliance.com>
2014-02-10 14:44:30 -0800

    Added tests for the YouTube API

--- test_youtube_api.py
+++ test_youtube_api.py
@@ -4,25 +4,44 @@ import config
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-#assert not config.YOUTUBE_API_KEY, "No YouTube API key specified - please add it to the config.py file"
+channel_id = None
+uploads_playlist = None
+youtube = None
 
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=config.YOUTUBE_API_KEY )
+def test_api_key():
+  pass
+  #assert not config.YOUTUBE_API_KEY, "No YouTube API key specified - please add it to the config.py file"
 
-search_response = youtube.channels().list(
-    part="contentDetails,id",
-    maxResults=10,
-    forUsername="mindcracknetwork",
-    fields="items/contentDetails/relatedPlaylists/uploads,items/id"
-  ).execute()
+def test_query_channel_information():
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=config.YOUTUBE_API_KEY )
 
-channel_id = search_response['items'][0]['id']
-uploads_playlist = search_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+  search_response = youtube.channels().list(
+      part="contentDetails,id",
+      maxResults=10,
+      forUsername="mindcracknetwork",
+      fields="items/contentDetails/relatedPlaylists/uploads,items/id"
+    ).execute()
 
-assert channel_id == 'UCAWQEAjn8udSFKN6D4NlqWQ', "Recieved unexpected channel ID"
-assert uploads_playlist == 'UUAWQEAjn8udSFKN6D4NlqWQ', "Recieved unexpected uploads playlist ID"
+  channel_id = search_response['items'][0]['id']
+  uploads_playlist = search_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-uploaded_videos = youtube.playlistItems().list(
-    part="id",
-    maxResults=10,
-    playlistId=uploads_playlist
-  ).execute()
+  assert channel_id == 'UCAWQEAjn8udSFKN6D4NlqWQ', "Recieved unexpected channel ID"
+  assert uploads_playlist == 'UUAWQEAjn8udSFKN6D4NlqWQ', "Recieved unexpected uploads playlist ID"
+
+def test_query_channel_videos():
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=config.YOUTUBE_API_KEY )
+  uploads_playlist = 'UUAWQEAjn8udSFKN6D4NlqWQ'
+
+  uploaded_videos = youtube.playlistItems().list(
+      part="id,snippet,contentDetails",
+      maxResults=20,
+      playlistId=uploads_playlist,
+      fields="items/id,items/snippet/title,items/snippet/description,items/contentDetails/videoId"
+    ).execute()
+
+  try:
+    for video in uploaded_videos['items']:
+      assert video['snippet']['title']
+      assert video['contentDetails']['videoId']
+  except KeyError:
+    assert False, "Missing id, snippet.title or contentDetails.videoId"
