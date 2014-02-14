dee2a65a4da1e8f4b57d78e1cb9216ab135c2d8c
Erik Granlund <egranlund@unifiedcompliance.com>
2014-02-14 14:22:47 -0800

    The run script now 1) Downloads 2) Uploads 3) Transcodes 4) Deletes (on source computer, not s3) - automatically!

--- run.py
+++ run.py
@@ -1,9 +1,25 @@
+import os
 from youtube import YouTubeChannel
+from amazon import Bucket, Transcoder
+
+s3_bucket = Bucket('egranlund.podcast')
+elastic_transcoder = Transcoder()
 
 channel = YouTubeChannel('mindcracknetwork')
 
-for video in channel.get_uploaded_videos(10):
+for video in channel.get_uploaded_videos(1):
   try:
-    video.download_audio()
+    filename = video.get_audio_filename()
+    
+    print "Downloading video"
+    video.download_audio( filename )
+    print "Uploading file"
+    s3_bucket.upload_file( filename )
+    print "Transcoding file"
+    elastic_transcoder.convert_to_mp3( filename, filename[:-3] + 'mp3' )
+
+    print "deleting file"
+    os.unlink( filename )
+
   except AttributeError:
     print "Unable to download '" + video.title + "'"
