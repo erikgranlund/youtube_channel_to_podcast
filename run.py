c86333a46bb402ec8770eb13bbe1070050248af7
Erik Granlund <egranlund@unifiedcompliance.com>
2014-02-14 17:04:12 -0800

    Got it downloading, uploading and transcoding the latest 5 episodes off of a YouTube channel

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ from amazon import Bucket, Transcoder
 
 import jinja2
 
+debug = False
+
 file_extension = 'mp3'
 youtube_channel = 'mindcracknetwork'
 
@@ -12,16 +14,17 @@ elastic_transcoder = Transcoder()
 
 channel = YouTubeChannel( youtube_channel )
 
-videos = channel.get_uploaded_videos(1)
+videos = channel.get_uploaded_videos(5)
 
 for video in videos:
   try:
     filename = video.get_audio_filename()
 
-#    video.download_audio( filename )
-#    s3_bucket.upload_file( filename )
-#    elastic_transcoder.convert_to_mp3( filename, filename[:-3] + file_extension )
-#    os.unlink( filename )
+    if debug == False:
+      video.download_audio( filename )
+      s3_bucket.upload_file( filename )
+      elastic_transcoder.convert_to_mp3( filename, filename[:-3] + file_extension )
+      os.unlink( filename )
  
     video.date = 'Wed, 6 Jul 2005 13:00:00 PDT'
     video.size = s3_bucket.get_size( filename )
